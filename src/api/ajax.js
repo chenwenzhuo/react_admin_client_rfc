b957fd6d1316bdc01fc969a0704c29f0aa1a5b4e
chenwenzhuo <Chenwz1996@163.com>
2022-11-17 23:15:16 +0800

    封装ajax请求，并对请求失败的情况在函数内部进行处理

---
+++ src/api/ajax.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import {message} from "antd";
+
+const ajaxMtd = (url, data = {}, method = 'GET') => {
+    return new Promise((resolve) => {
+        let promise;
+        //1.向后端发送GET/POST请求
+        if (method === 'GET') {
+            promise = axios.get('http://localhost:3000' + url, {params: data});
+        } else {
+            promise = axios.post('http://localhost:3000' + url, data);
+        }
+        promise.then(response => {//2.若请求成功（与后端通信成功），将promise置为成功状态
+            resolve(response.data);
+        }).catch(error => {//3.请求失败时，弹窗提示
+            console.log('----------------------请求出错----------------------');
+            console.log(error);
+            message.error('请求出错！' + error.message);
+        });
+    });
+}
+
+export default ajaxMtd;
