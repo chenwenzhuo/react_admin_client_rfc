06deed1893a9365ba29a9c79fa32cf693880a775
chenwenzhuo <Chenwz1996@163.com>
2022-11-17 23:40:15 +0800

    完善登陆页面

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Button, Form, Input} from 'antd';
+import {useNavigate} from "react-router-dom";
+import {Button, Form, Input, message} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
+
+import ajaxMtd from "../../api/ajax";
 
 import './login.less';
 import logo from '../../assets/img/logo.png';
 
 function Login() {
+    const navigate = useNavigate();//useNavigate Hook用于实现编程路由导航
+
+    const handleLoginSubmit = async loginData => {
+        //发送请求，进行登陆
+        const response = await ajaxMtd(
+            '/ajaxProxy/login',
+            {username: loginData.username, password: loginData.password},
+            'POST'
+        );
+        if (response.status === 0) {//登陆成功，跳转到主页
+            navigate('/');
+        } else {//登陆失败，弹窗提示
+            message.error(response.msg);
+        }
+    }
+
     return (
         <div className={'login-container'}>
             <div className={'login-header'}>
@@ -14,22 +34,30 @@ function Login() {
             <div className={'login-content'}>
                 <div className={'content-title'}>用户登陆</div>
                 <div className={'content-form'}>
-                    <Form name="basic" initialValues={{remember: true}}>
+                    <Form className={'content-form'} onFinish={handleLoginSubmit}>
                         <Form.Item name="username"
                                    rules={[{required: true, message: '请输入用户名'},
-                                       {min: 4, message: '密码长度不少于4位'},
-                                       {max: 12, message: '密码长度不多于12位'}]}>
-                            <Input/>
+                                       {min: 4, message: '用户名长度不少于4位'},
+                                       {max: 12, message: '用户名长度不多于12位'},
+                                       {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名应由字母、数字、下划线组成'}]}>
+                            <Input className={'form-input'}
+                                   prefix={<UserOutlined/>}
+                                   placeholder="用户名"/>
                         </Form.Item>
 
                         <Form.Item name="password"
-                                   rules={[{required: true, message: '请输入密码',},]}>
-                            <Input.Password/>
+                                   rules={[{required: true, message: '请输入密码'},
+                                       {min: 4, message: '用户名长度不少于4位'},
+                                       {max: 12, message: '用户名长度不多于12位'},
+                                       {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名应由字母、数字、下划线组成'}]}>
+                            <Input.Password className={'form-input'}
+                                            prefix={<LockOutlined/>}
+                                            placeholder="密码"/>
                         </Form.Item>
 
-                        <Form.Item wrapperCol={{offset: 8, span: 16,}}>
-                            <Button type="primary" htmlType="submit">
-                                Submit
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className={'login-button'}>
+                                登陆
                             </Button>
                         </Form.Item>
                     </Form>
