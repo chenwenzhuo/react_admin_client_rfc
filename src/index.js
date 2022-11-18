b27e466432b2a3007a8348949d5fd5bffcf5f142
chenwenzhuo <Chenwz1996@163.com>
2022-11-18 20:45:29 +0800

    引入redux，向应用传递store和持久化存储persistor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';//react18的写法
 import ReactDOM from 'react-dom';//react18以下的写法
-import './index.css';
+import {Provider} from 'react-redux'
+import {PersistGate} from "redux-persist/integration/react";
+import store from './redux/store'
+import {persistor} from "./redux/store";
 import App from './App';
+import './index.css';
 
 //react18的写法
 /*const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,8 +18,12 @@ root.render(
 
 //react18以下的写法
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        </PersistGate>
+    </Provider>,
     document.getElementById('root')
 );
