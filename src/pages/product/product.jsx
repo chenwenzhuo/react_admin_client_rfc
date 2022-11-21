44374ba2629aeb0ade6f6a9b913aeb21d832e316
chenwenzhuo <Chenwz1996@163.com>
2022-11-21 21:56:37 +0800

    修改product组件，编写子路由

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import './product.less';
+import {Navigate, Route, Routes} from "react-router-dom";
+import ProductHome from "./product-home";
+import ProductAddUpdate from "./product-add-update";
+import ProductDetail from "./product-detail";
 
-function Product(props) {
+function Product() {
+
     return (
-        <div>Product</div>
+        <Routes>
+            {/*使用exact属性为ProductHome开启严格匹配，否则二级路由无法匹配成功*/}
+            <Route path={''} element={<ProductHome/>}/>
+            <Route path={'add_update'} element={<ProductAddUpdate/>}/>
+            <Route path={'detail'} element={<ProductDetail/>}/>
+            <Route path={'*'} element={<Navigate to={''}/>}/>
+        </Routes>
     );
 }
 
