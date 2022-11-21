1463b81d1b27afdcd421c441b19b3d3fd0f0c820
chenwenzhuo <Chenwz1996@163.com>
2022-11-21 23:50:21 +0800

    引入样式文件

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -4,12 +4,12 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import ProductHome from "./product-home";
 import ProductAddUpdate from "./product-add-update";
 import ProductDetail from "./product-detail";
+import './product.less';
 
 function Product() {
 
     return (
         <Routes>
-            {/*使用exact属性为ProductHome开启严格匹配，否则二级路由无法匹配成功*/}
             <Route path={''} element={<ProductHome/>}/>
             <Route path={'add_update'} element={<ProductAddUpdate/>}/>
             <Route path={'detail'} element={<ProductDetail/>}/>
