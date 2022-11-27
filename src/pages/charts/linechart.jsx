d7735d02c9ced3e7edf66ffa26bc54ea9c84ed15
chenwenzhuo <Chenwz1996@163.com>
2022-11-27 22:25:46 +0800

    使用ReactECharts实现三种图表

--- src/pages/charts/linechart.jsx
+++ src/pages/charts/linechart.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Card, Button} from "antd";
+import ReactECharts from 'echarts-for-react';
 
-function LineChart(props) {
+function LineChart() {
+    const [sales, setSales] = useState([5, 20, 36, 10, 10, 20, 27]);
+    const [storages, setStorages] = useState([23, 67, 9, 12, 50, 21, 49]);
+
+    function updateData() {
+        setSales(sales => sales.map(sale => sale + 1));
+        setStorages(storages => storages.map(storage => storage - 1));
+    }
+
+    function getOption() {
+        return {
+            title: {text: 'Echarts示例'},
+            tooltip: {},
+            legend: {data: ['销量', '库存']},
+            xAxis: {data: ['衬衫', '羊毛衫', '雪纺衫', '牛仔裤', '衬衫', '高跟鞋', '袜子']},
+            yAxis: {},
+            series: [
+                {name: '销量', type: 'line', data: sales},
+                {name: '库存', type: 'line', data: storages},
+            ]
+        }
+    }
+
     return (
-        <div>LineChart</div>
+        <>
+            <Card>
+                <Button type={'primary'} onClick={updateData}>更新</Button>
+            </Card>
+            <Card title={'折线图'}>
+                <ReactECharts option={getOption()} style={{height: 400}} opts={{renderer: 'svg'}}/>
+            </Card>
+        </>
     );
 }
 
